dd560e280be6e990dd548cf6103e50f57d3e4124
Remmiemmyjr <emily@isimar.com>
2022-08-27 17:18:16 -0700

    fixed parallax to make it go the right way and not go past cam

--- YNA/Assets/Scripts/CameraColor.cs
+++ YNA/Assets/Scripts/CameraColor.cs
@@ -7,7 +7,7 @@ public class CameraColor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.GetComponent<Camera>().backgroundColor = Color.black;
     }
 
     // Update is called once per frame
--- YNA/Assets/Scripts/Parallax.cs
+++ YNA/Assets/Scripts/Parallax.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-private float length, startpos;
-public GameObject cam;
-public float parallaxEffect; 
+    private float length, startpos;
+    private float camPos;
+    public GameObject cam;
+    public float parallaxEffect; 
 
     // Start is called before the first frame update
     void Start()
     {
-        startpos = transform.position.x;
+        camPos = cam.transform.position.x;
+        
         length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
@@ -19,11 +21,25 @@ public float parallaxEffect;
     void FixedUpdate()
     {
         float temp = (cam.transform.position.x * (1 - parallaxEffect));
-        float dist = (cam.transform.position.x * parallaxEffect);
+        float deltaDist = (cam.transform.position.x - camPos) * parallaxEffect;
+        
 
-        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        startpos = transform.position.x;
+        float camDist = (cam.transform.position.x - transform.position.x);
 
-        if(temp > startpos + length) startpos += length;
-        else if (temp < startpos - length) startpos -= length;
+
+        if (camDist > length)
+        {
+            startpos += length;
+        }
+
+        else if (camDist < -length)
+        {
+            startpos -= length;
+        }
+
+        camPos = cam.transform.position.x;
+
+        transform.position = new Vector3(startpos + deltaDist, transform.position.y, transform.position.z);
     }
 }
