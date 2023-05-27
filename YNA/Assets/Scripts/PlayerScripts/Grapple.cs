a5dc3819e4ba01ceefe3cf46c35839fa2353e1f1
Cameron Myers <45009513+cameron-myers@users.noreply.github.com>
2023-05-26 18:32:23 -0700

    Revert "Oops, I deleted input reput in"
    This reverts commit 4183c65910e66fb6cb388ff4d5de696a7b4bcec3.

--- YNA/Assets/Scripts/PlayerScripts/Grapple.cs
+++ YNA/Assets/Scripts/PlayerScripts/Grapple.cs
@@ -12,7 +12,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.Tilemaps;
 
 public class Grapple : MonoBehaviour
