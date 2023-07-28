2ca027eb5df56854ff6a0412f53441e9929af361
Remmiemmyjr <emily@isimar.com>
2023-07-28 00:05:21 -0700

    making more progress on funny rope stuff

--- YNA/Assets/Scripts/MiscScripts/TetherRope.cs
+++ YNA/Assets/Scripts/MiscScripts/TetherRope.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,7 +11,13 @@ public class TetherRope : MonoBehaviour
     public GameObject lastNode;
     public GameObject targetPartner;
 
-    public float segmentDistance;
+    Queue<GameObject> Rope = new Queue<GameObject>();
+
+    public float segmentDist = 0.25f;
+    public float maxDist = 2.0f;
+    public float minDist = 0.3f;
+
+    float currDist;
     bool isTethered;
 
     // Start is called before the first frame update
@@ -18,12 +26,14 @@ public class TetherRope : MonoBehaviour
         lastNode = transform.gameObject;
         isTethered = false;
         GetComponent<HingeJoint2D>().enabled = false;
-        targetPartner.GetComponent<HingeJoint2D>().enabled = false;
     }
 
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            RemoveRopeSegment();
+        }
     }
 
     public void Tether(InputAction.CallbackContext ctx)
@@ -40,11 +50,13 @@ public class TetherRope : MonoBehaviour
         Vector2 segPos = targetPartner.transform.position - lastNode.transform.position;
         segPos.Normalize();
 
-        segPos *= segmentDistance;
+        segPos *= segmentDist;
         segPos += (Vector2)(lastNode.transform.position);
 
         GameObject newNode = Instantiate(ropeSegment, segPos, Quaternion.identity);
-        newNode.transform.parent = transform;
+        //Rope.PushFront(newNode);
+        Rope.Enqueue(newNode);
+
 
         lastNode.GetComponent<HingeJoint2D>().connectedBody = newNode.GetComponent<Rigidbody2D>();
 
@@ -53,12 +65,21 @@ public class TetherRope : MonoBehaviour
 
     void DestroyRope()
     {
-
+        
     }
 
     void RemoveRopeSegment()
     {
+        if (Rope.Count > 0)
+        {
+            GameObject prevNode = Rope.ElementAt(Rope.Count - 2);
+            prevNode.GetComponent<HingeJoint2D>().connectedBody = targetPartner.GetComponent<Rigidbody2D>();
+            
+            Destroy(Rope.Last());
+            Rope.Dequeue();
 
+            lastNode = prevNode;
+        }
     }
 
     IEnumerator ManageRope(bool tethered)
@@ -68,20 +89,19 @@ public class TetherRope : MonoBehaviour
             // If player should be tethered: --------------------------------------------
             case true:
                 GetComponent<HingeJoint2D>().enabled = true;
-                targetPartner.GetComponent<HingeJoint2D>().enabled = true;
-                //lastNode.GetComponent<HingeJoint2D>().connectedBody = targetPartner.GetComponent<Rigidbody2D>();
-                targetPartner.GetComponent<HingeJoint2D>().connectedAnchor = (Vector2)lastNode.transform.position;
 
-                while (Vector2.Distance(targetPartner.transform.position, lastNode.transform.position) > segmentDistance)
+                while (Vector2.Distance(targetPartner.transform.position, lastNode.transform.position) > segmentDist)
                 {
                     AddRopeSegment();
                 }
+                
+                lastNode.GetComponent<HingeJoint2D>().connectedBody = targetPartner.GetComponent<Rigidbody2D>();
+                lastNode.tag = "Last";
                 break;
 
             // If player should NOT be tethered: ----------------------------------------
             case false:
                 GetComponent<HingeJoint2D>().enabled = false;
-                targetPartner.GetComponent<HingeJoint2D>().enabled = false;
 
                 DestroyRope();
                 break;
