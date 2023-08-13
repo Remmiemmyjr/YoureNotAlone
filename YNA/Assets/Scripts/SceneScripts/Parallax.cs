91f5a4edf81c6e42ee61a022dc85d763d08cfd1e
CDLaMar <46691597+CoDaLa@users.noreply.github.com>
2023-08-13 13:19:03 -0700

    Updated Parallax T1

--- YNA/Assets/Scripts/SceneScripts/Parallax.cs
+++ YNA/Assets/Scripts/SceneScripts/Parallax.cs
@@ -31,12 +31,12 @@ public class Parallax : MonoBehaviour
     }
 
 
-        ////////////////////////////////////////////////////////////////////////
-        // START ===============================================================
-        void Start()
+    ////////////////////////////////////////////////////////////////////////
+    // START ===============================================================
+    void Start()
     {
         camPos = cam.transform.position.x;
-        
+
         length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
@@ -47,7 +47,7 @@ public class Parallax : MonoBehaviour
     {
         float temp = (cam.transform.position.x * (1 - parallaxEffect));
         float deltaDist = (cam.transform.position.x - camPos) * parallaxEffect;
-        
+
 
         startpos = transform.position.x;
         float camDist = (cam.transform.position.x - transform.position.x);
