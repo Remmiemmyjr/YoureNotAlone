fa2aa26e8e37537e4018e13f67761159732bcfe1
iceblue64 <92569146+iceblue64@users.noreply.github.com>
2023-08-13 15:31:09 -0700

    Shaders be working
    You know it

--- YNA/Assets/BoxStats.cs
+++ YNA/Assets/BoxStats.cs
@@ -1,14 +1,14 @@
 //*************************************************
 // Project: We're Tethered Together
 // File: BoxStats.cs
-// Author/s: Emmy Berg
+// Author/s: Emmy Berg, Mike Doeren
 //
 // Desc: Keeps track of box info
 //
 // Notes:
 // - 
 //
-// Last Edit: 7/16/2023
+// Last Edit: 8/4/2023
 //
 //*************************************************
 
@@ -18,8 +18,33 @@ using UnityEngine;
 
 public class BoxStats : MonoBehaviour
 {
+    ////////////////////////////////////////////////////////////////////////
+    // VARIABLES ===========================================================
+    public Material boxMat;
+    public Material outlineMat;
+    public Color canGrab;
+    public Color isGrabbed;
+    SpriteRenderer sr;
+    // *********************************************************************
+
     void Awake()
     {
         GetComponent<HingeJoint2D>().enabled = false;
+        sr = GetComponent<SpriteRenderer>();
+        SetNormalMat();
+    }
+
+    public void SetNormalMat()
+    {
+        sr.material = boxMat;
+    }
+
+    public void SetOutlineMat(bool colorSwap)
+    {
+        sr.material = outlineMat;
+        if (colorSwap)
+            outlineMat.SetColor("_Color_Outline", isGrabbed);
+        else
+            outlineMat.SetColor("_Color_Outline", canGrab);
     }
 }
