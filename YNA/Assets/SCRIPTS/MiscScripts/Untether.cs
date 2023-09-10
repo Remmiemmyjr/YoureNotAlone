0cd48ace9d4483a4bad1fe498b940c543f2280a7
Remmiemmyjr <emily@isimar.com>
2023-09-09 17:32:40 -0700

    Made a script to disable eyes :>

--- YNA/Assets/SCRIPTS/MiscScripts/Untether.cs
+++ YNA/Assets/SCRIPTS/MiscScripts/Untether.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Untether : MonoBehaviour
 {
+    public AudioSource iamhere;
+
+    [SerializeField]
+    private AudioClip eyembiance;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Partner")
         {
             Info.grapple.Tethered(false);
+            iamhere.PlayOneShot(eyembiance);
         }
     }
 }
