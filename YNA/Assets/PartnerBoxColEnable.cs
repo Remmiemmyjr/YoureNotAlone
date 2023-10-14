21b12f2ccd06b1c0ee53588f68090d0007361ba1
CoDaLa <46691597+CoDaLa@users.noreply.github.com>
2023-10-14 01:11:45 -0700

    Layer Settings Update

---
+++ YNA/Assets/PartnerBoxColEnable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartnerBoxColEnable : MonoBehaviour
+{
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        
+    }
+}
